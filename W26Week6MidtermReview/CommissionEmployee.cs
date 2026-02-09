61aa5a9d02547950d64019cd5c3bb487d6c1d1dd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-09 13:28:56 -0500

    comm emp class

---
+++ W26Week6MidtermReview/CommissionEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week6MidtermReview
+{
+    public class CommissionEmployee : Employee
+    {
+        public double GrossSales { get; set; }
+        public double CommissionRate { get; set; }
+
+        public CommissionEmployee(string name, double grossSales, double commissionRate) : base(name)
+        {
+            GrossSales = grossSales;
+            CommissionRate = commissionRate;
+        }
+
+        public override double GrossEarnings()
+        {
+            return GrossSales * CommissionRate;
+        }
+    }
+}
